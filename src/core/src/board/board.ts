abc50662e28968d14922e8520307793f38b5a22c
George Spanos <gespa11019@gmail.com>
2022-10-16 19:48:16 +0300

    simplify board type

--- src/core/src/board/board.ts
+++ src/core/src/board/board.ts
@@ -3,11 +3,6 @@ import { Card } from '../card/card';
 export interface BoardBrand {
   readonly BoardBrand: unique symbol;
 }
-export const Board = t.brand(
-  t.array(Card),
-  (board: Card[]): board is t.Branded<Card[], BoardBrand> =>
-    board.length >= 0 && board.length <= 5,
-  'BoardBrand'
-);
+export const Board = t.array(Card);
 
-export type Board = t.TypeOf<typeof Board> | [];
+export type Board = t.TypeOf<typeof Board>;
