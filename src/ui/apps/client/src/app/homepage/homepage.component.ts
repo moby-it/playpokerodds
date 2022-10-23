64e506d1526ebd7fd3c2a5afb95dc518bf531c0f
George Spanos <gespa11019@gmail.com>
2022-10-23 20:05:18 +0300

    add home demo

--- src/ui/apps/client/src/app/homepage/homepage.component.ts
+++ src/ui/apps/client/src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,8 +6,15 @@ import { Router } from '@angular/router';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
-export class HomepageComponent {
+export class HomepageComponent implements AfterViewInit {
+  @ViewChild('demoVideo') video: ElementRef<HTMLVideoElement> | undefined;
   constructor(private router: Router) {}
+  ngAfterViewInit(): void {
+    if (this.video) {
+      this.video.nativeElement.muted = true;
+      this.video.nativeElement.play();
+    }
+  }
   navigateToPlay(): void {
     this.router.navigate(['/play']);
   }
