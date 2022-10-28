d6d8c12ab8c1e20de0a4d525b7299dc187c7bd0e
George Spanos <gespa11019@gmail.com>
2022-10-28 21:27:39 +0300

    add auth to az function

--- src/api/src/routes/poker/postRoundAnswer/calculateWinOdds.ts
+++ src/api/src/routes/poker/postRoundAnswer/calculateWinOdds.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { NextFunction, Request, Response } from 'express';
+import { pipe } from 'fp-ts/lib/function';
 import { tryCatch } from 'fp-ts/lib/TaskEither';
 import { PostAnswerDto } from './answer.dto';
 export const calculateWinOdds = (
@@ -9,13 +10,19 @@ export const calculateWinOdds = (
 ) => {
   tryCatch(
     () =>
-      axios
-        .post('https://ppocalcodds.azurewebsites.net/api/calcodds', {
-          round: res.locals.dto.round,
-        })
-        .then(response => {
-          res.locals.odds = response.data;
-        }),
+      pipe(process.env['CALC_ODDS_KEY'] ?? '', calcOddsKey =>
+        axios
+          .post(
+            `https://ppocalcodds.azurewebsites.net/api/calcodds?code=${calcOddsKey}`,
+            {
+              round: res.locals.dto.round,
+            }
+          )
+          .then(response => {
+            res.locals.odds = response.data;
+          })
+      ),
+
     e => res.status(400).send(e)
   )().then(() => next());
 };
