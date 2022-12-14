3093332bb679a2bea59c9fcd29ec4febfd6c31b0
George Spanos <gespa11019@gmail.com>
2022-12-14 14:29:22 +0200

    add extensive testing for multiple users and answers

--- src/web-api/tests/functional/post-existing-round-answer.spec.ts
+++ src/web-api/tests/functional/post-existing-round-answer.spec.ts
@@ -1,19 +1,29 @@
 import { config } from 'dotenv';
 import express, { Application } from 'express';
+import { isRight } from 'fp-ts/lib/Either';
 import { registerMiddleware } from 'middleware';
 import prisma from 'prisma';
-import { PokerRouter } from 'routes';
+import { AuthRouter, PokerRouter, RoundAnswerResponse } from 'routes';
 import request from 'supertest';
-import { ExistingRoundPayloads, NewRoundPayloads } from '../fixtures';
+import {
+  ExistingRoundPayloads,
+  mockUserPayload1,
+  mockUserPayload2,
+  NewRoundPayloads,
+} from '../fixtures';
 import { mockDb } from '../helpers';
 describe('test post existing round answer endpoint', () => {
   let roundId: '';
+  const tokens: string[] = [];
+  const usernames: string[] = [];
+  const userScores: number[] = [];
   let app: Application;
   beforeAll(async () => {
     config();
     app = express();
     registerMiddleware(app);
     app.use(PokerRouter);
+    app.use(AuthRouter);
   });
   afterAll(async () => {
     await mockDb.tearDown(prisma);
@@ -37,6 +47,28 @@ describe('test post existing round answer endpoint', () => {
       .send(ExistingRoundPayloads.postRoundInvalidPayload3)
       .expect(400);
   });
+  it('should register users', async () => {
+    await request(app)
+      .post('/register')
+      .send(mockUserPayload1)
+      .expect(200)
+      .expect(res => {
+        expect('token' in res.body).toBeTruthy();
+        tokens.push(res.body.token);
+        expect('username' in res.body).toBeTruthy();
+        usernames.push(res.body.username);
+      });
+    await request(app)
+      .post('/register')
+      .send(mockUserPayload2)
+      .expect(200)
+      .expect(res => {
+        expect('token' in res.body).toBeTruthy();
+        tokens.push(res.body.token);
+        expect('username' in res.body).toBeTruthy();
+        usernames.push(res.body.username);
+      });
+  });
   it('should play a new Round', async () => {
     await request(app)
       .post('/postNewRoundAnswer')
@@ -46,25 +78,49 @@ describe('test post existing round answer endpoint', () => {
         expect('id' in res.body).toBeTruthy();
         roundId = res.body.id;
       });
-    expect(await prisma.event.count()).toEqual(1);
+    expect(await prisma.event.count()).toEqual(3);
     expect(await prisma.round.count()).toEqual(1);
   });
-  it('should post valid round payload', async () => {
+  it('should post valid round payload with auth', async () => {
     await request(app)
       .post('/postExistingRoundAnswer')
       .send({ ...ExistingRoundPayloads.postValidRoundPayload1, roundId })
-      .expect(200);
-    expect(await prisma.event.count()).toEqual(2);
+      .auth(tokens[0], { type: 'bearer' })
+      .expect(200)
+      .expect(response => {
+        expect('score' in response.body).toBeTruthy();
+        userScores.push(Math.abs(response.body.score));
+      });
+    expect(await prisma.event.count()).toEqual(4);
     expect(await prisma.round.count()).toEqual(1);
     expect(await prisma.roundAnswer.count()).toEqual(2);
   });
+  it('should have correctly updated user score', async () => {
+    const user = await prisma.user.findFirst({
+      where: { username: usernames[0] },
+    });
+    expect(user).toBeDefined();
+    expect(Math.abs(Number(user?.score))).toEqual(Math.abs(userScores[0]));
+  });
   it('should post valid round payload', async () => {
     await request(app)
       .post('/postExistingRoundAnswer')
       .send({ ...ExistingRoundPayloads.postValidRoundPayload1, roundId })
-      .expect(200);
-    expect(await prisma.event.count()).toEqual(3);
+      .auth(tokens[1], { type: 'bearer' })
+      .expect(200)
+      .expect(response => {
+        expect('score' in response.body).toBeTruthy();
+        userScores.push(Math.abs(response.body.score));
+      });
+    expect(await prisma.event.count()).toEqual(5);
     expect(await prisma.round.count()).toEqual(1);
     expect(await prisma.roundAnswer.count()).toEqual(3);
+  });
+  it('should have correctly updated user score', async () => {
+    const user = await prisma.user.findFirst({
+      where: { username: usernames[1] },
+    });
+    expect(user).toBeDefined();
+    expect(Math.abs(Number(user?.score))).toEqual(Math.abs(userScores[1]));
   });
 });
