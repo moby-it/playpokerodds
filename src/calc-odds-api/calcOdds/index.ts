8fe01ee7d7ab68ec83e1e4652b2148853a0b3ef7
George Spanos <gespa11019@gmail.com>
2022-12-19 15:57:14 +0200

    change functions to work with default axios json parse

--- src/calc-odds-api/calcOdds/index.ts
+++ src/calc-odds-api/calcOdds/index.ts
@@ -1,23 +1,20 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import {
-  calculateOdds,
-  Round
-} from "@moby-it/ppo-core";
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { calculateOdds, Round } from '@moby-it/ppo-core';
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   const inputs = Round.decode(req.body?.round);
-  if (inputs._tag === "Left") {
+  if (inputs._tag === 'Left') {
     context.res = {
       status: 400,
-      body: "input hand invalid format",
+      body: 'input hand invalid format',
     };
   } else {
     const odds = calculateOdds(inputs.right);
     context.res = {
-      body: odds,
+      body: { odds },
     };
   }
 };
