f81f42a7b811e5c518c457ffe9ca267ac876090d
George Spanos <gespa11019@gmail.com>
2022-12-22 16:25:32 +0200

    remove legacy paginator

--- src/ui/apps/admin/src/app/dashboard/dashboard.component.ts
+++ src/ui/apps/admin/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
+import { MatTableDataSource } from '@angular/material/table';
 import { combineLatest, map } from 'rxjs';
 import { AppEvent, DashboardStore } from './dashboard.store';
 import { format } from 'date-fns';
