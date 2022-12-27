c0ed74051209c1852e319e740315ee8814ffe534
George Spanos <gespa11019@gmail.com>
2022-12-27 21:42:19 +0200

    update calc-odds-serverless to new core lib

--- src/calc-odds-api-serverless/calcOdds/index.ts
+++ src/calc-odds-api-serverless/calcOdds/index.ts
@@ -1,25 +1,22 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import {
-  calculateOdds,
-  Round
-} from "@moby-it/ppo-core";
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { calculateOdds, validateRound } from '@moby-it/ppo-core';
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  const inputs = Round.decode(req.body?.round);
-  if (inputs._tag === "Left") {
+  const round = req.body?.round;
+  if (!validateRound(round)) {
     context.res = {
       status: 400,
-      body: "input hand invalid format",
+      body: 'input hand invalid format',
     };
-  } else {
-    const odds = calculateOdds(inputs.right);
-    context.res = {
-      body: odds,
-    };
+    return;
   }
+  const odds = calculateOdds(round);
+  context.res = {
+    body: odds,
+  };
 };
 
 export default httpTrigger;
