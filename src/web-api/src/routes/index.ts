0710c7d840fad4c0c93c404d35322afc31cb11b2
George Spanos <gespa11019@gmail.com>
2023-01-03 00:46:20 +0200

    add healthcheck endpoint

--- src/web-api/src/routes/index.ts
+++ src/web-api/src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Application } from 'express';
+import { Application, Request, Response } from 'express';
 import { AuthRouter } from './auth';
 import { PokerRouter } from './poker';
 export { AuthRouter, PokerRouter };
@@ -6,4 +6,7 @@ export { RoundAnswerResponse } from './poker';
 export function registerAppRoutes(app: Application) {
   app.use('/auth', AuthRouter);
   app.use('/poker', PokerRouter);
+  app.use('/health', (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
 }
