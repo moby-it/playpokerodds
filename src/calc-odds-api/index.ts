89975479e62432c1b973c73999ee8bddabc0fc3e
George Spanos <gespa11019@gmail.com>
2023-01-04 18:03:44 +0200

    add calc odds cors and profile confing on web api

--- src/calc-odds-api/index.ts
+++ src/calc-odds-api/index.ts
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 7071;
-app.use(cors());
+app.use(cors({
+  origin:'https://playpokerodds.com',
+}));
 app.use(bodyParser.json());
 
 app.post('/api/calcOdds', (req: Request, res: Response) => {
