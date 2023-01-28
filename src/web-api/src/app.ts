392aec7f9ab06c2d54845d41a377328f09b638b2
George Spanos <gespa11019@gmail.com>
2023-01-28 15:26:37 +0200

    remove dynamic port on web-api

--- src/web-api/src/app.ts
+++ src/web-api/src/app.ts
@@ -7,7 +7,7 @@ config();
 const app: Application = express();
 registerMiddleware(app);
 registerRoutes(app);
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 registerErrorHandlers(app);
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
