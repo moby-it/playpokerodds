7d0cb7eacec4316caf47ca47b1df322883cca5cb
George Spanos <gespa11019@gmail.com>
2023-02-01 11:46:42 +0200

    user profile stylings

---
+++ src/ui/libs/user/ui/src/lib/side-menu/side-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'ppo-user-profile-side-menu',
+  templateUrl: './side-menu.component.html',
+  standalone: true,
+  imports: [RouterModule],
+})
+export class SideMenuComponent {}
--- src/ui/libs/user/ui/src/lib/user-profile-container.component.ts
+++ src/ui/libs/user/ui/src/lib/user-profile-container.component.ts
@@ -1,14 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PushModule } from '@ngrx/component';
+import { AuthFacade } from '@ppo/auth/domain';
 import { PokerOddsFacade } from '@ppo/game/domain';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 @Component({
   selector: 'ppo-user-profile-container',
   templateUrl: './user-profile-container.component.html',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SideMenuComponent, PushModule],
 })
 export class UserProfileContainerComponent {
-  constructor() {
+  constructor(private auth: AuthFacade) {
     console.log(inject(PokerOddsFacade));
   }
+  username$ = this.auth.username$;
 }
