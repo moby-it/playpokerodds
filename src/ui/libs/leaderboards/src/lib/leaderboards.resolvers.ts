e15eab455bc0cda703bb6a0f1980e4456be4065a
George Spanos <gespa11019@gmail.com>
2023-02-04 01:31:18 +0200

    fix loader issue

--- src/ui/libs/leaderboards/src/lib/leaderboards.resolvers.ts
+++ src/ui/libs/leaderboards/src/lib/leaderboards.resolvers.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
-import { LoaderService } from '@ppo/shared/ui';
-import { map, Observable, skipWhile } from 'rxjs';
+import { Observable, skipWhile } from 'rxjs';
 import { LeaderboardsStore } from './leaderboards.store';
 
 @Injectable()
-export class LeaderboardsResolver implements Resolve<void> {
-  constructor(
-    private leaderboardStore: LeaderboardsStore,
-    private loader: LoaderService
-  ) {}
-  resolve(): Observable<void> {
-    this.loader.setLoading(true);
-    return this.leaderboardStore.fetchLeaderboards().pipe(
-      skipWhile((loading) => !loading),
-      map(() => {
-        this.loader.setLoading(false);
-      })
-    );
+export class LeaderboardsResolver implements Resolve<unknown> {
+  constructor(private leaderboardStore: LeaderboardsStore) {}
+  resolve(): Observable<unknown> {
+    return this.leaderboardStore
+      .fetchLeaderboards()
+      .pipe(skipWhile((loading) => !loading));
   }
 }
