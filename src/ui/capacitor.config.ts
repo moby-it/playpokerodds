08b411763b8256dabeac5c163db3e7974377fa17
George Spanos <gespa11019@gmail.com>
2023-02-12 19:28:43 +0200

    add android port

---
+++ src/ui/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.playpokerodds.app',
+  appName: 'playpokerodds',
+  webDir: 'dist/apps/client',
+  bundledWebRuntime: false
+};
+
+export default config;
