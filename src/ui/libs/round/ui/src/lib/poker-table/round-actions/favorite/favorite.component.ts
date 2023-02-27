20f5614a265cdbc6e3ca4a9330463b0ca60b6aa9
George Spanos <gespa11019@gmail.com>
2023-02-27 17:43:29 +0200

    reduce dockerfiles footprint and fix favorites icon issue not filling

--- src/ui/libs/round/ui/src/lib/poker-table/round-actions/favorite/favorite.component.ts
+++ src/ui/libs/round/ui/src/lib/poker-table/round-actions/favorite/favorite.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { PokerOddsFacade } from '@ppo/play/domain';
 import { SharedUiModule } from '@ppo/shared/ui';
+import { BehaviorSubject } from 'rxjs';
+import { PushModule } from '@ngrx/component';
 @Component({
   selector: 'ppo-add-round-to-favorites-button',
   template: ` <svg
@@ -15,7 +17,7 @@ import { SharedUiModule } from '@ppo/shared/ui';
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      [attr.fill]="favoritesFill"
+      [attr.fill]="favoritesFill | ngrxPush"
       d="M11.9874 10.7611L15.422 3.47874C15.8629 2.54387 17.1366 2.54387 17.5775 3.47874L21.0121 10.7611L28.6928 11.9361C29.6783 12.0868 30.071 13.3547 29.3576 14.0819L23.8008 19.7465L25.1122 27.749C25.2807 28.7767 24.25 29.5604 23.3682 29.0751L16.4998 25.2946L9.63134 29.0751C8.74957 29.5604 7.71887 28.7767 7.88728 27.749L9.19868 19.7465L3.64195 14.0819C2.9285 13.3547 3.32127 12.0868 4.30674 11.9361L11.9874 10.7611Z"
       stroke="#FAFAFB"
       stroke-width="2"
@@ -31,21 +33,21 @@ import { SharedUiModule } from '@ppo/shared/ui';
     `,
   ],
   standalone: true,
-  imports: [CommonModule, SharedUiModule],
+  imports: [CommonModule, SharedUiModule, PushModule],
 })
 export class FavoriteButtonComponent {
-  favoritesFill = 'transparent';
+  favoritesFill = new BehaviorSubject('transparent');
   constructor(private pokerOdds: PokerOddsFacade) {}
   @Input() roundId: string | undefined;
   saveToFavorites(): void {
     if (this.roundId) {
-      if (this.favoritesFill === 'transparent') {
+      if (this.favoritesFill.getValue() === 'transparent') {
         this.pokerOdds.addRoundToFavorites(this.roundId).subscribe(() => {
-          this.favoritesFill = 'white';
+          this.favoritesFill.next('white');
         });
       } else {
         this.pokerOdds.removeRoundFromFavorites(this.roundId).subscribe(() => {
-          this.favoritesFill = 'transparent';
+          this.favoritesFill.next('transparent');
         });
       }
     }
