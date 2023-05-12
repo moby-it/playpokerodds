6afe8b0064eb653c5119e0e6fa1d81a3a414770f
George Spanos <gespa11019@gmail.com>
2023-05-12 19:57:11 +0300

    paginated events

--- src/ui/apps/admin/src/app/dashboard/dashboard.store.ts
+++ src/ui/apps/admin/src/app/dashboard/dashboard.store.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
-import { EMPTY, filter, map, Observable, switchMap } from 'rxjs';
+import { EMPTY, Observable, combineLatest, debounceTime, filter, map, switchMap, timer, withLatestFrom } from 'rxjs';
 import { environment } from '../../environments/environment';
 export enum EventType {
   USER_REGISTERED = 'USER_REGISTERED',
@@ -10,7 +10,11 @@ export enum EventType {
   USER_POSTED_ANSWER = 'USER_POSTED_ANSWER',
   UNKNOWN_ERROR = 'UNKNOWN_ERROR',
 }
-
+export type Filters = {
+  pageSize: number,
+  page: number;
+  term: string;
+};
 export interface AppEvent {
   id: string;
   type: string;
@@ -19,7 +23,9 @@ export interface AppEvent {
 }
 interface DashboardEventsState {
   events: AppEvent[];
+  total: number;
   tableReady: boolean;
+  filters: Filters;
   last24hLogins: number;
   last24hRoundsPlayed: number;
 }
@@ -31,13 +37,29 @@ export class DashboardStore extends ComponentStore<DashboardEventsState> {
       tableReady: false,
       last24hLogins: 0,
       last24hRoundsPlayed: 0,
+      total: 0,
+      filters: {
+        page: 0,
+        pageSize: 10,
+        term: ''
+      }
     });
-    this.fetchEvents$();
-    setInterval(() => this.fetchEvents$(), 30000);
+    combineLatest([timer(0, 30 * 1000), this.filters$]).pipe(debounceTime(500)).subscribe(() => {
+      this.fetchEvents$();
+    });
   }
   events$ = this.select((state) => state.events).pipe(
     filter((e) => !!e.length)
   );
+  filters$ = this.select((state) => state.filters);
+  pagination$ = this.select(state => ({ total: state.total, pageSize: state.filters.pageSize, pageIndex: state.filters.page }));
+  updateTerm = this.updater((state, term: string) => ({
+    ...state, filters: { ...state.filters, term, page: 0 }
+  }));
+  changePagination = this.updater((state, pagination: { pageSize: number, page: number; }) => ({
+    ...state, filters: { ...state.filters, page: pagination.page, pageSize: pagination.pageSize }
+  }));
+  private updateTotal = this.updater((state, total: number) => ({ ...state, total }));
   last24hRoundsPlayed$ = this.events$.pipe(
     map(
       (events) =>
@@ -79,9 +101,16 @@ export class DashboardStore extends ComponentStore<DashboardEventsState> {
   }));
   private fetchEvents$ = this.effect((source$: Observable<void>) =>
     source$.pipe(
-      switchMap(() => this.http.get(environment.apiUrl + '/poker/fetchEvents')),
+      withLatestFrom(this.filters$),
+      map(([, filters]) =>
+        new HttpParams().set('page', filters.page + 1).set('pageSize', filters.pageSize).set('term', filters.term)
+      ),
+      switchMap((params: HttpParams) => this.http.get<{ events: AppEvent[], total: number; }>(environment.apiUrl + '/poker/fetchEvents', { params })),
       tapResponse(
-        (e) => this.setEvents(e as AppEvent[]),
+        ({ events, total }) => {
+          this.setEvents(events as AppEvent[]);
+          this.updateTotal(total);
+        },
         (e: HttpErrorResponse) => {
           console.error(e);
           return EMPTY;
--- src/web-api/src/routes/poker/fetchEvents/fetchEvents.ts
+++ src/web-api/src/routes/poker/fetchEvents/fetchEvents.ts
@@ -1,13 +1,37 @@
-import { NextFunction, Request, Response } from 'express';
+import { Event, Prisma } from '@prisma/client';
+import { Request, Response } from 'express';
 import prisma from 'prisma';
 
+(BigInt.prototype as any).toJSON = function () {
+  try {
+    return Number(this);
+  } catch (e) {
+    return this.toString();
+  }
+};
+/**
+ * Fetch paginated events endpoint.
+ * @QueryParam pageSize Query Param
+ * @QueryParam page Query Param - page index
+ * @QueryParam term Query Param - search term
+ */
 export async function fetchEvents(
   req: Request,
   res: Response,
-  next: NextFunction
 ) {
-  const events = await prisma.event.findMany({
-    orderBy: { timestamp: 'desc' },
-  });
-  res.status(200).send(events);
+  const pageSize = parseInt(req.query.pageSize as string) || 10;
+  const page = parseInt(req.query.page as string) || 1;
+  const term: string = String(req.query.term) || '' as string;
+  const offset = (page - 1) * pageSize;
+  let r = 'select *, count(*) over() as total from "Events"';
+  if (term) {
+    r += `where payload::text like '%${term}%'`;
+  }
+  r += `order by timestamp desc
+    limit ${pageSize}
+    offset ${offset}`;
+  const events: Array<Event & { total?: number; }> = await prisma.$queryRaw(Prisma.raw(r));
+  const total = events[0]?.total || 0;
+  events.forEach(e => delete e.total);
+  res.status(200).send({ events, total });
 }
