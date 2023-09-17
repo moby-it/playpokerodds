63ff1f4e7d22f8d0cef8190c07707b5f418fc044
George Spanos <gspanos@moby-it.com>
2023-09-17 11:17:49 +0300

    unless you big hon

--- install.sh
+++ install.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# remember to have a valid npm_token file in your project root directory
+token=$(cat npm_token)
+
+n=${#token}
+
+if ! command -v bun &> /dev/null
+then
+    echo "bun could not be found"
+    exit 1
+fi
+
+echo "reading github token..."
+if [ "$n" -eq "0" ]; then
+  echo "Failed to read github token.Create a file named npm_token and paste inside the give github token";
+  exit 1;
+fi
 
 replace_token() {
-  sed -i "s/\${NPM_TOKEN}/$(cat ../../npm_token)/" .npmrc && npm ci && sed -i "s/$(cat ../../npm_token)/\${NPM_TOKEN}/" .npmrc 
+  sed -i "s/{NPM_TOKEN}/$token/" bunfig.toml && bun install && sed -i "s/$token/{NPM_TOKEN}/" bunfig.toml
 }
+
+copy_bunfig(){
+  cp bunfig.toml src/ui/
+  cp bunfig.toml src/calc-odds-api
+  cp bunfig.toml src/web-api
+}
+
+copy_bunfig
+
 cd src/calc-odds-api
 replace_token
 
@@ -15,4 +38,4 @@ cd ../web-api
 replace_token
 
 cd ../core
-npm ci
+bun install
