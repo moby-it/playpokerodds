ca9390ef3633a2b03061cb4cd67832254840c9c9
George Spanos <gspanos@moby-it.com>
2023-12-14 18:22:11 +0200

    remove cors from api

--- src/web-api/src/middleware/index.ts
+++ src/web-api/src/middleware/index.ts
@@ -1,11 +1,9 @@
 import bodyParser from 'body-parser';
 import { Application } from 'express';
 import morgan from 'morgan';
-import cors from 'cors';
 import { registerPassportMiddleware } from './passport';
 
 export function registerMiddleware(app: Application) {
-  app.use(cors());
   app.use(bodyParser.json());
   app.use(morgan('dev'));
   registerPassportMiddleware();
