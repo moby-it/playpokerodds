48182bfd2a49ebb65f2f381faa61a0d5bd2aca60
George Spanos <gspanos@moby-it.com>
2023-12-14 14:08:25 +0200

    remove api url
    ui apps should always hit their own domain. Cors should be enabled by default. UIs should sit behind a reverse proxy.

--- src/ui/apps/admin/src/environments/environment.prod.ts
+++ src/ui/apps/admin/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'https://api.playpokerodds.com',
+  apiUrl: '/api',
 };
