6dedcc1ceaa88401b9bd43c430d056c45a26806c
George Spanos <gspanos@moby-it.com>
2024-02-27 03:49:47 +0200

    refactor: calc-odds deno v1

---
+++ src/calc-odds-deno/main.ts
@@ -0,0 +1,36 @@
+import { calculateOdds } from "@poker-core/calculate-odds/calculate-odds.ts";
+import { validateRound, Round } from "@poker-core/round/round.ts";
+import { Application, Context, Next, Router } from "https://deno.land/x/oak@v14.0.0/mod.ts";
+const port = Number(Deno.env.get('PORT') || '7071');
+
+let iterations = Number(Deno.env.get('ITERATIONS'));
+
+if (!iterations || iterations <= 0) {
+  console.error('iterations invalid value. Will use default value: 50_000');
+  iterations = 50_000;
+}
+
+const apiKey = Deno.env.get('APIKEY');
+
+const router = new Router();
+router.post('/api/calcOdds', async (ctx) => {
+  const body: { estimate: number, round: Round; } = await ctx.request.body.json();
+  if (!validateRound(body.round)) {
+    ctx.throw(400, 'invalid round payload');
+    return;
+  }
+  const odds = calculateOdds(body.round, iterations);
+  ctx.response.body = { odds };
+});
+const app = new Application();
+app.use(validateClient);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port });
+
+function validateClient(ctx: Context, next: Next) {
+  if (!apiKey) return next();
+  else if (apiKey === ctx.request.headers.get('x-api-key')) next();
+  else ctx.response.status = 403;
+}
